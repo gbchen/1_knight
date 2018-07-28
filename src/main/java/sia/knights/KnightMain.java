08da6c15e14f0381ea35a27afbd5dc598bfab65e
cgb <chenguibao@corp.netease.com>
2018-07-28 10:29:58 +0800

    @dev

--- src/main/java/sia/knights/KnightMain.java
+++ src/main/java/sia/knights/KnightMain.java
@@ -1,17 +1,14 @@
 package sia.knights;
 
-import org.springframework.context.support.
-                   ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 
-  public static void main(String[] args) throws Exception {
-    ClassPathXmlApplicationContext context =
-        new ClassPathXmlApplicationContext(
-            "META-INF/spring/knight.xml");
-    Knight knight = context.getBean(Knight.class);
-    knight.embarkOnQuest();
-    context.close();
-  }
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
+        Knight knight = context.getBean(Knight.class);
+        knight.embarkOnQuest();
+        context.close();
+    }
 
 }
